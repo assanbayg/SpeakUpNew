da99c4dd0f16769482b31aebe1f73f6a4662a242
Gauhar <asanbayg@gmail.com>
2025-08-18 14:43:16 +0500

    chore: clean up Google Maps integration

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,7 +7,6 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDH_d0mTQtiA4y4C4tQj1N3ux2ELEEsGvY") // Add this line with your new API key
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/features/speakup/screens/map_screen.dart
+++ lib/features/speakup/screens/map_screen.dart
@@ -6,6 +6,7 @@ import 'package:speakup/common/widgets/appbar.dart';
 import 'package:speakup/features/speakup/screens/converter_screen.dart';
 import 'package:speakup/features/speakup/screens/home_screen.dart';
 import 'package:speakup/features/speakup/screens/profile_page.dart';
+import 'package:speakup/util/data/marker_coords.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key, required this.text});
@@ -67,39 +68,6 @@ class _MapScreenState extends State<MapScreen> {
     );
   }
 
-  // Store all coordinates in a LatLng list 
-  // TODO: move this to a separate file
-  final List<LatLng> markerCoords = [
-    const LatLng(51.1684126, 71.4377708),
-    const LatLng(51.110174, 71.4405484),
-    const LatLng(51.1458321, 71.391254),
-    const LatLng(51.1404791, 71.4816291),
-    const LatLng(51.1318099, 71.4431659),
-    const LatLng(51.1584428, 71.4392943),
-    const LatLng(51.1645947, 71.4210839),
-    const LatLng(51.1141434, 71.419799),
-    const LatLng(51.0968369, 71.4283003),
-    const LatLng(51.1318099, 71.4431659),
-    const LatLng(51.1318099, 71.4431659),
-    const LatLng(51.1318099, 71.4431659),
-    const LatLng(51.1318099, 71.4431659),
-    const LatLng(51.1318099, 71.4431659),
-    const LatLng(43.2279509, 76.9298164),
-    const LatLng(43.2483956, 76.9242436),
-    const LatLng(43.2588596, 76.9215298),
-    const LatLng(43.2647393, 76.9418947),
-    const LatLng(43.1954553, 76.9166263),
-    const LatLng(43.2607363, 76.9382604),
-    const LatLng(43.2631766, 76.9407591),
-    const LatLng(43.2647393, 76.9418947),
-    const LatLng(43.2625185, 76.917988),
-    const LatLng(43.259426, 76.923469),
-    const LatLng(43.2531557, 76.9462131),
-    const LatLng(43.2441716, 76.9029286),
-    const LatLng(43.257839, 76.936721),
-    const LatLng(43.2491872, 76.9153096),
-  ];
-
   String dropdownValue = 'Алматы';
 
   void _onDropDownChanged(String? city) {
@@ -141,9 +109,9 @@ class _MapScreenState extends State<MapScreen> {
             ),
             children: [
               TileLayer(
-                urlTemplate:
-                    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                 subdomains: const ['a', 'b', 'c'],
+                userAgentPackageName: 'com.devoasis.speakup',
               ),
               MarkerLayer(markers: markers),
             ],
