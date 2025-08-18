4a8a7e5ee530400793c1f4f6288525f2f070c8ad
Gauhar <asanbayg@gmail.com>
2025-08-18 23:23:44 +0500

    refactor: clean up Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,23 @@
-import 'package:firebase_app_check/firebase_app_check.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:speakup/features/authentication/screens/login_screen.dart';
 import 'package:speakup/features/speakup/controllers/text_to_speech_controller.dart';
-import 'package:speakup/features/speakup/screens/profile_page.dart';
-import 'package:speakup/firebase_options.dart';
-import 'package:speakup/util/helpers/firebase_hepler.dart';
+import 'package:speakup/features/speakup/screens/home_screen.dart';
+import 'package:speakup/util/helpers/supabase_helper.dart';
 import 'package:speakup/util/theme/theme.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-// TODO: FIX BUG
-// when signing out and signing in, i get some TTS error
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   await Supabase.initialize(
     url: dotenv.env['SUPABASE_URL']!,
     anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
 
-  // TODO: remove when cleaning up repo
-  await FirebaseAppCheck.instance.activate(
-    androidProvider: AndroidProvider.debug,
-    appleProvider: AppleProvider.debug,
-  );
+  // TODO: FIX when signing out and signing in, i get some TTS error
   Get.lazyPut(() => TextToSpeechController());
 
   runApp(const SpeakUp());
@@ -43,10 +31,8 @@ class SpeakUp extends StatelessWidget {
     return GetMaterialApp(
       theme: STheme.sTheme,
       debugShowCheckedModeBanner: false,
-      home: SFireHelper.fireAuth.currentUser != null
-          // ? const HomeScreen()
-          // ? const MapScreen(text: "")
-          ? const UserProfilePage()
+      home: SSupabaseHelper.currentUser != null
+          ? const HomeScreen()
           : const LoginScreen(),
     );
   }
